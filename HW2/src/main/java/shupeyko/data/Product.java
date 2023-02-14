8b79a82ef4a0394a99dd09267cf8c81b752bdb23
Andrey <andryws@mail.ru>
2023-02-14 16:22:23 +0300

    Spring 2. Home Work 2.

---
+++ HW2/src/main/java/shupeyko/data/Product.java
@@ -0,0 +1,27 @@
+package shupeyko.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "cost")
+    private Integer cost;
+}
